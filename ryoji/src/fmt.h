b1b167510a755ecc428cda6bf23cbf808c12f689
GERALD WONG <ramza88@gmail.com>
2020-05-24 22:14:58 +0800

    removed fixedFormat

--- ryoji/src/fmt.h
+++ ryoji/src/fmt.h
@@ -5,13 +5,13 @@
 #include <assert.h>
 
 namespace ryoji::fmt {
-    template<size_t BufferSize, typename ... Args>
+    /*template<size_t BufferSize, typename ... Args>
     std::string fixedFormat(const std::string& format, Args ... args)
     {
         char buffer[BufferSize];
         int count = snprintf(buffer, BufferSize, format.c_str(), args ...);
         return std::string(buffer); // We don't want the '\0' inside
-    }
+    }*/
 }
 
 
