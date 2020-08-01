b20a84e8e0324928c1ef65dfebd57d598ab064aa
GERALD WONG <ramza88@gmail.com>
2020-08-02 02:49:32 +0800

    update

--- code/game_mode_sandbox.h
+++ code/game_mode_sandbox.h
@@ -5,38 +5,43 @@
 #include "game.h"
 #include "game_assets.h"
 
-
-
-struct test_entity {
+// NOTE(Momo): EC....S?
+struct sandbox_transform_component {
     v3f Scale;
     f32 Rotation;
     v3f Position;
+};
+
+struct sandbox_renderable_component {
     c4f Colors;
     u32 TextureHandle;
+    quad2f TextureCoords;
+};
+
+// TODO(Momo): Animation component
+
+struct sandbox_entity {
+    sandbox_transform_component Transform;
+    sandbox_renderable_component Renderable;
     f32 Timer;
 };
 
 
-static inline void
-Update(test_entity* Entity, 
-       game_assets* Assets,
-       render_commands * RenderCommands, 
-       f32 DeltaTime) {
-    
-    // NOTE(Momo): Update
-    
-    Entity->Rotation +=  DeltaTime;
-    
+
+static inline void RenderingSystem(sandbox_transform_component* Transform, sandbox_renderable_component* Renderable,
+                                   render_commands* RenderCommands,
+                                   game_assets* Assets) 
+{
     // NOTE(Momo): Render
-    m44f T = TranslationMatrix(Entity->Position);
-    m44f R = RotationZMatrix(Entity->Rotation);
-    m44f S = ScaleMatrix(Entity->Scale);
+    m44f T = TranslationMatrix(Transform->Position);
+    m44f R = RotationZMatrix(Transform->Rotation);
+    m44f S = ScaleMatrix(Transform->Scale);
     
     // TODO(Momo): This part should be done by renderer?
     PushCommandTexturedQuad(RenderCommands, 
-                            Entity->Colors, 
+                            Renderable->Colors, 
                             T*R*S,
-                            Assets->Textures[Entity->TextureHandle],
+                            Assets->Textures[Renderable->TextureHandle],
                             {
                                 0.5f, 0.5f, // top right
                                 0.5f, 0.f, // bottom right
@@ -45,11 +50,27 @@ Update(test_entity* Entity,
                             });
 }
 
+static inline void
+Update(sandbox_entity* Entity, 
+       game_assets* Assets,
+       render_commands * RenderCommands, 
+       f32 DeltaTime) {
+    
+    // NOTE(Momo): Update
+    Entity->Transform.Rotation +=  DeltaTime;
+    
+    RenderingSystem(&Entity->Transform, 
+                    &Entity->Renderable, 
+                    RenderCommands, 
+                    Assets);
+    
+}
+
 
 struct game_mode_sandbox {
     static constexpr u8 TypeId = 2;
     static constexpr u32 TotalEntities = 2500;
-    test_entity Entities[TotalEntities];
+    sandbox_entity Entities[TotalEntities];
 };
 
 
@@ -62,20 +83,25 @@ InitMode(game_mode_sandbox* Mode, game_state* GameState) {
     f32 offsetDeltaY = 5.f;
     for (u32 i = 0; i < game_mode_sandbox::TotalEntities; ++i)
     {
-        Mode->Entities[i].Position = { offsetX, offsetY, 0.f };
-        Mode->Entities[i].Rotation = 0.f;
-        Mode->Entities[i].Scale = { 5.f, 5.f };
-        Mode->Entities[i].Colors = { 1.f, 1.f, 1.f, 0.5f };
-#if 1
+        Mode->Entities[i].Transform.Position = { offsetX, offsetY, 0.f };
+        Mode->Entities[i].Transform.Rotation = 0.f;
+        Mode->Entities[i].Transform.Scale = { 5.f, 5.f };
+        
+        Mode->Entities[i].Renderable.Colors = { 1.f, 1.f, 1.f, 0.5f };
+        Mode->Entities[i].Renderable.TextureCoords = {
+            0.0f, 1.0f,
+            1.0f, 1.0f,
+            0.0f, 0.0f,
+            1.0f, 0.0f
+        };
+        
         if ( i < (game_mode_sandbox::TotalEntities / 3))
-            Mode->Entities[i].TextureHandle = 1;
+            Mode->Entities[i].Renderable.TextureHandle = 1;
         else if ( i < (game_mode_sandbox::TotalEntities / 3 * 2))
-            Mode->Entities[i].TextureHandle = 2;
+            Mode->Entities[i].Renderable.TextureHandle = 2;
         else if ( i < game_mode_sandbox::TotalEntities)
-            Mode->Entities[i].TextureHandle = 0;
-#else
-        Mode->Entities[i].TextureHandle = i % 3;
-#endif
+            Mode->Entities[i].Renderable.TextureHandle = 0;
+        
         offsetX += offsetDeltaX;
         if (offsetX >= 700.f) {
             offsetX = -700.f;
@@ -108,15 +134,5 @@ UpdateMode(game_mode_sandbox* Mode,
                DeltaTime);
     }
 }
-
-
-
-
-
-
-
-
-
-
 
 #endif //GAME_MODE_SANDBOX_H
