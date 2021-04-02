f7937855bb1a383843f92d8202b6d4c4f16357be
Gerald Wong <email@geraldwong.net>
2021-04-02 10:36:44 +0800

    updated

--- bin/bg.bat
+++ bin/bg.bat
@@ -1,4 +1,5 @@
 @echo off
+
 SET me=%~dp0
 
 SET RootDir=%me%..
@@ -6,8 +7,9 @@ SET CodeDir=%RootDir%\code
 SET BuildDir=%RootDir%\build
 SET Opt=%1
 
-IF NOT "%Platform%" == "X64" IF NOT "%Platform%" == "x64" (EXIT /b)
 call %me%..\bin\setup_cl_x64.bat
+IF NOT "%Platform%" == "X64" IF NOT "%Platform%" == "x64" (EXIT /b)
+
 
 SET CommonCompilerFlags=-MT -WX -W4 -wd4189 -wd4702 -wd4201 -wd4505 -wd4996 -wd4100 -Zi  -GR -EHa  -std:c++17
 SET CommonCompilerFlags=-DSLOW -DINTERNAL  %CommonCompilerFlags%
