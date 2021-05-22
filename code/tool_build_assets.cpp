c99de8d462761aea2a2ec1e70e7f0a4301b7609c
Gerald Wong <email@geraldwong.net>
2021-05-22 11:39:15 +0800

    Revert "updated"
    This reverts commit c1b5c5840e9bf3e9da9e62db71f9826daf2044d9.

--- code/tool_build_assets.cpp
+++ code/tool_build_assets.cpp
@@ -22,7 +22,7 @@
 
 #define Tba_GenerateTestPng 1
 #define Tba_MemorySize Megabytes(8)
-#define Tba_MemCheck printf("[Memcheck] Line %d: %d bytes used\n", __LINE__, Arena.used)
+#define Tba_MemCheck printf("[Memcheck] Line %d: %d bytes used\n", __LINE__, Arena.Used)
 
 
 int main() {
@@ -35,8 +35,7 @@ int main() {
     }
     Defer { free(Memory); };
     
-    MM_Arena Arena = {};
-    MM_Arena_Init(&Arena, Memory, Tba_MemorySize);
+    MM_Arena Arena = Arena_Create(Memory, Tba_MemorySize);
     
     //~ NOTE(Momo): Load font
     
