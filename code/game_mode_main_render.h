aa21b7c09871e5f122cfdc3c69bbe89917af399c
Gerald Wong <email@geraldwong.net>
2021-05-22 21:34:56 +0800

    updated

--- code/game_mode_main_render.h
+++
@@ -1,9 +0,0 @@
-/* date = May 9th 2021 5:43 pm */
-
-#ifndef GAME_MODE_MAIN_RENDER_H
-#define GAME_MODE_MAIN_RENDER_H
-
-
-
-
-#endif //GAME_MODE_MAIN_RENDER_H
--- code/game_mode_main_update.h
+++
@@ -1,7 +0,0 @@
-/* date = May 9th 2021 5:43 pm */
-
-#ifndef GAME_MODE_MAIN_UPDATE_H
-#define GAME_MODE_MAIN_UPDATE_H
-
-
-#endif //GAME_MODE_MAIN_UPDATE_H
